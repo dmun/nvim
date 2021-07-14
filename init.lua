1e0ea611f092a10af29d563dcef86b586a7ce137
David van Munster <davidru01@live.nl>
2021-07-14 15:30:22 +0200

    Change require('plugins') priority

--- init.lua
+++ init.lua
@@ -1,8 +1,8 @@
--- Vim settings
-vim.cmd('source $HOME/.config/nvim/.vimrc')
-
 -- Plugins
 require('plugins')
+
+-- Vim settings
+vim.cmd('source $HOME/.config/nvim/.vimrc')
 
 -- Plugin settings
 require('settings')
