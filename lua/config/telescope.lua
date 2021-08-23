31d8a42141319ce552f2c79d12eeb2fe3de757c0
David van Munster <davidru01@live.nl>
2021-08-23 21:38:39 +0200

    change telescope

--- lua/config/telescope.lua
+++ lua/config/telescope.lua
@@ -1,4 +1,14 @@
-map('n', '<leader>ff', [[:lua require('telescope.builtin').find_files()<CR>]], { silent = true })
-map('n', '<leader>fg', [[:lua require('telescope.builtin').live_grep()<CR>]], { silent = true })
-map('n', '<leader>fb', [[:lua require('telescope.builtin').buffers()<CR>]], { silent = true })
+map('n', '<leader><leader>', [[:lua require('telescope.builtin').find_files()<CR>]], { silent = true })
+map('n', '<leader>/', [[:lua require('telescope.builtin').live_grep()<CR>]], { silent = true })
+map('n', '<leader>,', [[:lua require('telescope.builtin').buffers()<CR>]], { silent = true })
+map('n', '<leader>.', [[:lua require('telescope.builtin').file_browser()<CR>]], { silent = true })
 map('n', '<leader>fh', [[:lua require('telescope.builtin').help_tags()<CR>]], { silent = true })
+
+require'telescope'.setup {
+	pickers = {
+		find_files = {
+			theme = 'dropdown',
+			previewer = false,
+		},
+	}
+}
