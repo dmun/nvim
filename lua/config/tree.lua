ecfac193e3ee7c7fdbc4584e763a75131f2d187e
dmun <davidru01@live.nl>
2022-04-27 19:28:54 +0200

    Remove deprecated

--- lua/config/tree.lua
+++ lua/config/tree.lua
@@ -6,12 +6,7 @@ require'nvim-tree'.setup {
   hijack_netrw        = true,
   open_on_setup       = false,
   ignore_ft_on_setup  = {},
-  auto_close          = false,
   open_on_tab         = false,
-  update_to_buf_dir   = {
-    enable = true,
-    auto_open = true,
-  },
   hijack_cursor       = true,
   update_cwd          = true,
   update_focused_file = {
@@ -28,7 +23,6 @@ require'nvim-tree'.setup {
     width = 30,
     height = 30,
     side = 'left',
-    auto_resize = false,
     mappings = {
       custom_only = false,
       list = {
