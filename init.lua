8156f9b173799e9eb445b189589842365ba0b092
dmun <davidru01@live.nl>
2022-05-26 19:02:01 +0200

    Fix packer

--- init.lua
+++ init.lua
@@ -6,3 +6,19 @@ require "defaults"
 
 -- Load plugins.
 require "plugins"
+
+-- Load configs (temporary).
+require "core.cmp"
+require "core.colorizer"
+require "core.gitsigns"
+require "core.indentline"
+require "core.lspconfig"
+require "core.lspinstall"
+require "core.lspkind"
+require "core.lualine"
+require "core.null-ls"
+require "core.packer"
+require "core.pairs"
+require "core.telescope"
+require "core.tree"
+require "core.treesitter"
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,70 +1,60 @@
 vim.cmd "packadd packer.nvim"
 
 return require("packer").startup {
-	function()
-		use { "wbthomason/packer.nvim", config = require "core.packer" }
-		use "lewis6991/impatient.nvim"
+    function()
+        use "wbthomason/packer.nvim"
+        use "lewis6991/impatient.nvim"
 
-		--	Colors
-		use {
-			"norcalli/nvim-colorizer.lua",
-			config = function()
-				require "core.colorizer"
-			end,
-		}
-		use { "dmun/doom-one.nvim", branch = "personal" }
+        --	Colors
+        use "norcalli/nvim-colorizer.lua"
+        use { "dmun/doom-one.nvim", branch = "personal" }
 
-		--	Git
-		use { "lewis6991/gitsigns.nvim", config = require "core.gitsigns" }
+        --	Git
+        use "lewis6991/gitsigns.nvim"
 
-		--	LSP
-		use { "neovim/nvim-lspconfig", config = require "core.lspconfig" }
-		use { "williamboman/nvim-lsp-installer", config = require "core.lspinstall" }
-		use { "onsails/lspkind-nvim", config = require "core.lspkind" }
-		use { "jose-elias-alvarez/null-ls.nvim", config = require "core.null-ls" }
-		use {
-			"numToStr/Comment.nvim",
-			config = function()
-				require("Comment").setup()
-			end,
-		}
+        --	LSP
+        use "neovim/nvim-lspconfig"
+        use "williamboman/nvim-lsp-installer"
+        use "onsails/lspkind-nvim"
+        use "jose-elias-alvarez/null-ls.nvim"
+        use {
+            "numToStr/Comment.nvim",
+            config = function()
+                require("Comment").setup()
+            end,
+        }
 
-		--  Autocomplete
-		use { "hrsh7th/nvim-cmp", config = require "core.cmp" }
-		use "hrsh7th/cmp-buffer"
-		use "hrsh7th/cmp-nvim-lua"
-		use "hrsh7th/cmp-nvim-lsp"
-		use "L3MON4D3/LuaSnip"
-		use "saadparwaiz1/cmp_luasnip"
-		use { "rafamadriz/friendly-snippets", config = require("luasnip/loaders/from_vscode").lazy_load() }
+        --  Autocomplete
+        use "hrsh7th/nvim-cmp"
+        use "hrsh7th/cmp-buffer"
+        use "hrsh7th/cmp-nvim-lua"
+        use "hrsh7th/cmp-nvim-lsp"
+        use "L3MON4D3/LuaSnip"
+        use "saadparwaiz1/cmp_luasnip"
+        use { "rafamadriz/friendly-snippets", config = require("luasnip/loaders/from_vscode").lazy_load() }
 
-		--	IDE like
-		use { "nvim-treesitter/nvim-treesitter", config = require "core.treesitter" }
-		use { "kyazdani42/nvim-tree.lua", config = require "core.tree" }
-		use { "nvim-telescope/telescope.nvim", config = require "core.telescope" }
-		use "ahmedkhalf/project.nvim"
-		use {
-			"windwp/nvim-autopairs",
-			config = function()
-				require("nvim-autopairs").setup()
-			end,
-		}
+        --	IDE like
+        use "nvim-treesitter/nvim-treesitter"
+        use "kyazdani42/nvim-tree.lua"
+        use "nvim-telescope/telescope.nvim"
+        use "ahmedkhalf/project.nvim"
+        use "windwp/nvim-autopairs"
 
-		--	Appearance
-		use "kyazdani42/nvim-web-devicons"
-		use { "nvim-lualine/lualine.nvim", config = require "core.lualine" }
-		use { "lukas-reineke/indent-blankline.nvim", config = require "core.indentline" }
+        --	Appearance
+        use "kyazdani42/nvim-web-devicons"
+        use "nvim-lualine/lualine.nvim"
+        use "lukas-reineke/indent-blankline.nvim"
 
-		--	Misc
-		use "airblade/vim-rooter"
-		use "nvim-lua/popup.nvim"
-		use "nvim-lua/plenary.nvim"
-	end,
-	config = {
-		display = {
-			open_fn = function()
-				return require("packer.util").float { border = "rounded" }
-			end,
-		},
-	},
+        --	Misc
+        use "airblade/vim-rooter"
+        use "nvim-lua/popup.nvim"
+        use "nvim-lua/plenary.nvim"
+    end,
+    config = {
+        display = {
+            open_fn = function()
+                return require("packer.util").float { border = "none" }
+            end,
+        },
+    },
 }
