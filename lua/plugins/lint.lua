2805713673924bc99c13d34bb0496afc41d12ab9
dmun <davidru01@live.nl>
2024-04-10 15:19:20 +0200

    refactor lint

--- lua/plugins/lint.lua
+++ lua/plugins/lint.lua
@@ -2,12 +2,15 @@ return {
 	"mfussenegger/nvim-lint",
 	event = "BufWritePost",
 	config = function()
-		require("lint").linters_by_ft = {
+		local lint = require("lint")
+
+		lint.linters_by_ft = {
 			lua = { "luacheck" },
 		}
+
 		vim.api.nvim_create_autocmd("InsertLeave", {
 			callback = function()
-				require("lint").try_lint()
+				lint.try_lint()
 			end,
 		})
 	end,
