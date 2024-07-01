601c66364620162568651f7f1a52a6378c6a0c2a
dmun <davidru01@live.nl>
2024-07-01 17:28:57 +0200

    y

--- lua/plugins/files.lua
+++ lua/plugins/files.lua
@@ -1,29 +1,28 @@
-Plug("airblade/vim-rooter")
+Plug('airblade/vim-rooter')
 
-Plug("stevearc/oil.nvim")
-	:dependencies("nvim-tree/nvim-web-devicons")
-	:keys { { "-", "<cmd>Oil<cr>" } }
-	:opts {
+Plug('stevearc/oil.nvim')
+	:dependencies('nvim-tree/nvim-web-devicons')
+	:keys { { '-', '<cmd>Oil<cr>' } }:opts {
 		view_options = {
 			is_hidden_file = function(name, bufnr)
-				return vim.startswith(name, ".") or vim.endswith(name, ".pdf")
-			end
-		}
+				return vim.startswith(name, '.') or vim.endswith(name, '.pdf')
+			end,
+		},
 	}
 
-Plug("ibhagwan/fzf-lua")
-	:cmd("FzfLua")
+Plug('ibhagwan/fzf-lua')
+	:cmd('FzfLua')
 	:keys {
-		{ "<leader>f", "<cmd>FzfLua files<cr>" },
-		{ "<leader>l", "<cmd>FzfLua oldfiles<cr>" },
-		{ "<leader>/", "<cmd>FzfLua live_grep<cr>" },
-		{ "<leader>?", "<cmd>FzfLua live_grep_resume<cr>" },
-		{ "<leader>,", "<cmd>FzfLua buffers<cr>" },
-		{ "<leader><leader>", "<cmd>FzfLua builtin<cr>" },
+		{ '<leader>f', '<cmd>FzfLua files<cr>' },
+		{ '<leader>l', '<cmd>FzfLua oldfiles<cr>' },
+		{ '<leader>/', '<cmd>FzfLua live_grep<cr>' },
+		{ '<leader>?', '<cmd>FzfLua live_grep_resume<cr>' },
+		{ '<leader>,', '<cmd>FzfLua buffers<cr>' },
+		{ '<leader><leader>', '<cmd>FzfLua builtin<cr>' },
 	}
 	:opts {
 		files = {
-			cmd = "rg --files --hidden",
+			cmd = 'rg --files --hidden',
 			no_header_i = true,
 			file_icons = false,
 			-- git_icons = false,
@@ -31,43 +30,46 @@ Plug("ibhagwan/fzf-lua")
 		grep = { no_header_i = true },
 		buffers = { no_header_i = true },
 		winopts = {
-			split = "botright new",
-			preview = { hidden = "hidden" }
-		}
+			split = 'botright new',
+			preview = { hidden = 'hidden' },
+		},
+		fzf_opts = {
+			['--no-info'] = true,
+		},
 	}
 
-Plug("cbochs/grapple.nvim")
+Plug('cbochs/grapple.nvim')
 	:dependencies {
-		{ "nvim-tree/nvim-web-devicons", lazy = true },
+		{ 'nvim-tree/nvim-web-devicons', lazy = true },
 	}
 	:keys {
-		{ "<leader>q", "<cmd>Grapple toggle_tags<cr>" },
-		{ "<leader>m", "<cmd>Grapple tag<cr>" },
-		{ "<leader>1", "<cmd>Grapple select index=1<cr>" },
-		{ "<leader>2", "<cmd>Grapple select index=2<cr>" },
-		{ "<leader>3", "<cmd>Grapple select index=3<cr>" },
-		{ "<leader>4", "<cmd>Grapple select index=4<cr>" },
-		{ "<leader>5", "<cmd>Grapple select index=5<cr>" },
+		{ '<leader>q', '<cmd>Grapple toggle_tags<cr>' },
+		{ '<leader>m', '<cmd>Grapple tag<cr>' },
+		{ '<leader>1', '<cmd>Grapple select index=1<cr>' },
+		{ '<leader>2', '<cmd>Grapple select index=2<cr>' },
+		{ '<leader>3', '<cmd>Grapple select index=3<cr>' },
+		{ '<leader>4', '<cmd>Grapple select index=4<cr>' },
+		{ '<leader>5', '<cmd>Grapple select index=5<cr>' },
 	}
 	:opts {
-		scope = "cwd",
+		scope = 'cwd',
 	}
 
-Plug("nvim-tree/nvim-tree.lua")
+Plug('nvim-tree/nvim-tree.lua')
 	:disabled()
 	:keys {
-		{ "<leader>e", "<cmd>NvimTreeOpen<cr>" },
+		{ '<leader>e', '<cmd>NvimTreeOpen<cr>' },
 	}
 	:opts {
 		view = {
 			cursorline = false,
 		},
 		on_attach = function(bufnr)
-			local api = require("nvim-tree.api")
+			local api = require('nvim-tree.api')
 
 			local function opts(desc)
 				return {
-					desc = "nvim-tree: " .. desc,
+					desc = 'nvim-tree: ' .. desc,
 					buffer = bufnr,
 					noremap = true,
 					silent = true,
@@ -77,7 +79,7 @@ Plug("nvim-tree/nvim-tree.lua")
 
 			api.config.mappings.default_on_attach(bufnr)
 
-			vim.keymap.set("n", "<M-e>", api.tree.close, opts("Close"))
-			vim.keymap.set("n", "?", api.tree.toggle_help, opts("Help"))
+			vim.keymap.set('n', '<M-e>', api.tree.close, opts('Close'))
+			vim.keymap.set('n', '?', api.tree.toggle_help, opts('Help'))
 		end,
 	}
