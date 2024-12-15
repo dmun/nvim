89df86440104177108389a778659de58dda502c7
dmun <davidru01@live.nl>
2024-12-15 20:41:25 +0100

    fix ru

--- lua/configs/lsp.lua
+++ lua/configs/lsp.lua
@@ -5,6 +5,33 @@ Conf.lsp = {
 
 Conf.ls.kotlin_lsp = {}
 
+Conf.ls.pyright = {
+	settings = {
+		pyright = {
+			disableOrganizeImports = true,
+		},
+		python = {
+			ignore = { "*" },
+			typeCheckingMode = "off",
+		},
+	},
+}
+
+vim.api.nvim_create_autocmd("LspAttach", {
+	group = vim.api.nvim_create_augroup("lsp_attach_disable_ruff_hover", { clear = true }),
+	callback = function(args)
+		local client = vim.lsp.get_client_by_id(args.data.client_id)
+		if client == nil then
+			return
+		end
+		if client.name == "ruff" then
+			-- Disable hover in favor of Pyright
+			client.server_capabilities.hoverProvider = false
+		end
+	end,
+	desc = "LSP: Disable hover capability from Ruff",
+})
+
 Conf.ls.zls = {
 	on_init = function(client, _)
 		client.server_capabilities.semanticTokensProvider = nil
