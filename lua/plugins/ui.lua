9d24cc504db74c453d458ea326b1d7a922fcb48e
David van Munster <davidru01@live.nl>
2025-05-25 13:57:09 +0200

    f

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -37,8 +37,8 @@ starter.setup({
   end,
   evaluate_single = true,
   items = {
-    starter.sections.recent_files(4, false, true),
-    starter.sections.recent_files(4, true, true),
+    starter.sections.recent_files(4, false, false),
+    starter.sections.recent_files(4, true, false),
     -- starter.sections.sessions(4),
     {
       { action = MiniDeps.update,    name = "u. update",    section = "Deps" },
