0942ed5246205a0c27394c32f4e2cba3562faf0f
David van Munster <davidru01@live.nl>
2025-06-17 15:32:59 +0200

    f

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,5 +1,12 @@
 local lsp = vim.lsp
 
+add({
+  source = "mason-org/mason-lspconfig.nvim",
+  depends = { "mason-org/mason.nvim", "neovim/nvim-lspconfig" },
+})
+require("mason").setup()
+require("mason-lspconfig").setup()
+
 add("folke/lazydev.nvim")
 require("lazydev").setup({
   library = {
